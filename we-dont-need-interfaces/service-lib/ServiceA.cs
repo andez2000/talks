7cea6f0a7b1560d91d9f9851493fac5963337f9e
Paul Anderson <paul.anderson@legendware.co.uk>
2020-03-04 08:43:13 +0000

    initial add

---
+++ we-dont-need-interfaces/service-lib/ServiceA.cs
@@ -0,0 +1,16 @@
+﻿namespace service_lib
+{
+    public class ServiceA : IServiceA
+    {
+        public string HitItChewie()
+        {
+            return "Wooooooooooo hoooooooooooooooo!!!!";
+        }
+    }
+
+    public interface IServiceA
+    {
+        string HitItChewie();
+    }
+
+}
---
+++ we-dont-need-interfaces/service-lib/ServiceB.cs
@@ -0,0 +1,23 @@
+﻿namespace service_lib
+{
+    public class ServiceB : IServiceB
+    {
+        private readonly IServiceA serviceA;
+
+        public ServiceB(IServiceA serviceA)
+        {
+            this.serviceA = serviceA;
+        }
+
+        public string HitItChewie()
+        {
+            return serviceA.HitItChewie();
+        }
+    }
+
+    public interface IServiceB
+    {
+        string HitItChewie();
+    }
+
+}
